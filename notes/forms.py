b766bb2ae5e374ebde6a4fcd53dfd178e069488f
acchewan <acchewan@gmail.com>
2026-02-18 07:48:03 +0545

    Enhance NoteForm validation and add tests for note creation

--- notes/forms.py
+++ notes/forms.py
@@ -12,5 +12,12 @@ class NoteForm(forms.Form):
         error_messages={'required': "Description is required"}
     )
 
+    def clean_description(self):
+        description = self.cleaned_data.get('description', '')
+        if len(description) < 10:
+            raise forms.ValidationError(
+                'Description must be at least 10 characters long.')
+        return description
+
     def create(self, validated_data):
         return Note.objects.create(**validated_data)
